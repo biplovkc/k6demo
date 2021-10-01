66f23ddda5fcf15c7a117bc60d068ebdaaa221f8
Biplov <biplov.cybercop@gmail.com>
2021-10-01 17:50:23 +0200

    added check for result status code

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 import http from 'k6/http';
-
+import {check} from 'k6';
 export default function (){
     const result = http.get("http://test-api.k6.io");
+
+    check(result, {
+        'Result code is 200': (r) => r.status === 200,
+    });
 }
