e923248830d626df417360558af7d442c2fd0ea2
Biplov <biplov.cybercop@gmail.com>
2021-10-01 17:57:42 +0200

    send result to cloud

--- test.js
+++ test.js
@@ -5,9 +5,17 @@ import { Rate } from 'k6/metrics';
 const failures = new Rate('failed requests');
 
 export const options = {
+    ext:{
+        loadimpact:{
+            projectID: '<your project id in k6 cloud>',
+            name: 'K6 demo',
+        },
+    },
+    vus:10,
+    duration: '5s',
     thresholds: {
         failed_requests: ['rate<=0'],
-        http_req_duration: ['p(95)<100'],
+        http_req_duration: ['p(95)<100', 'p(99)<500'],
     }
 }
 export default function (){
