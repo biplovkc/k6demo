9a0fc546c80f7c88f71b8a39832507a0c2f2cabc
Biplov <biplov.cybercop@gmail.com>
2021-10-01 17:52:44 +0200

    added rate to validate p(95) response times

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 import http from 'k6/http';
 import {check} from 'k6';
+import { Rate } from 'k6/metrics';
+
+const failures = new Rate('failed requests');
+
+export const options = {
+    thresholds: {
+        failed_requests: ['rate<=0'],
+        http_req_duration: ['p(95)<100'],
+    }
+}
 export default function (){
     const result = http.get("http://test-api.k6.io");
 
