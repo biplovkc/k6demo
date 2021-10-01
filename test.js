bc8745e86b27ff2197cc113664ed0e5937e8f717
Biplov <biplov.cybercop@gmail.com>
2021-10-01 18:16:22 +0200

    multiple scenarios

--- test.js
+++ test.js
@@ -11,17 +11,53 @@ export const options = {
             name: 'K6 demo',
         },
     },
+    scenarios: {
+        test: {
+            executor: "constant-arrival-rate",
+            rate: 10,
+            timeUnit: "1s",
+            duration: "30s",
+            preAllocatedVUs: 1,
+            maxVUs: 10
+        },
+        foo: {
+            executor: "constant-arrival-rate",
+            rate: 15,
+            exec: "foo",
+            timeUnit: "2s",
+            duration: "10s",
+            preAllocatedVUs: 1,
+            maxVUs: 10,
+        },
+    },
     vus:10,
-    duration: '5s',
     thresholds: {
         failed_requests: ['rate<=0'],
         http_req_duration: ['p(95)<100', 'p(99)<500'],
     }
 }
-export default function (){
+
+export default function () {
     const result = http.get("http://test-api.k6.io");
 
-    check(result, {
-        'Result code is 200': (r) => r.status === 200,
-    });
+    checkResult(result, "default", 200);
+    failures.add(result.status != 200);
+}
+
+export function foo () {
+    const result = http.get("http://test-api.k6.io");
+
+    checkResult(result, "foo", 200);
+    failures.add(result.status != 200);
+}
+
+function checkResult(res, tag, resCode){
+    check(
+        res,
+        {
+            ['status' + tag + ' is ' + resCode + ' (Ok)']: (r) =>
+            r.status === resCode,
+        },
+        { my_tag: tag }
+    );
 }
